ce6a25dbe844fdd7a068ba782e79b4fc51859fe0
hj1024.jung <flksignal@gmail.com>
2023-07-06 14:27:20 +0900

    refac. getScoreByAlpha

--- Project2/SimilarityChecker.cpp
+++ Project2/SimilarityChecker.cpp
@@ -2,29 +2,79 @@
 
 using namespace std;
 
+void SetBit(int& bitmap, int index)
+{
+	bitmap |= (1 << index);
+}
+
+bool CheckBit(int bitmap, int index)
+{
+	return bitmap & (1 << index);
+}
+
 class SimilarityChecker
 {
 public:
+	static const int MAX_SCORE_BY_ALPHA = 40;
+
+	int getAlphaIndex(char c)
+	{
+		return (c - 'A');
+	}
+
+	int getTotalAlphaCount(const string& s1, const string& s2)
+	{
+		int count = 0;
+		int AlphaCheckBitmap = 0;
+		string str = s1 + s2;
+
+		for (char c : str)
+		{
+			int index = getAlphaIndex(c);
+
+			if (CheckBit(AlphaCheckBitmap, index)) continue;
+
+			SetBit(AlphaCheckBitmap, index);
+			count++;			
+		}
+
+		return count;
+	}
+
+	bool hasSameChar(const string& str, char ch)
+	{
+		for (char c : str)
+		{
+			if (c == ch)
+				return true;
+		}
+		return false;
+	}
+
+	int getSameAlphaCount(const string& s1, const string& s2)
+	{
+		int count = 0;
+		int AlphaCheckBitmap = 0;
+
+		for (char c : s1)
+		{
+			int index = getAlphaIndex(c);
+
+			if (CheckBit(AlphaCheckBitmap, index)) continue;
+
+			SetBit(AlphaCheckBitmap, index);
+
+			if (hasSameChar(s2, c))
+			{
+				count++;
+			}			
+		}
+
+		return count;
+	}
 
 	int getScoreByAlpha(const string& s1, const string& s2)
 	{
-		if ((s1 == "ASD") && (s2 == "DSA"))
-		{
-			return 40;
-		}
-		if ((s1 == "A") && (s2 == "BB"))
-		{
-			return 0;
-		}
-		if ((s1 == "AAABB") && (s2 == "BAA"))
-		{
-			return 40;
-		}
-		if ((s1 == "AA") && (s2 == "AAE"))
-		{
-			return 20;
-		}
-
-		return 0;
+		return MAX_SCORE_BY_ALPHA *	getSameAlphaCount(s1, s2) / getTotalAlphaCount(s1, s2);
 	}
 };
