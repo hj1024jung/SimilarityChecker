47aaf58eef5519242c5a1e3dc2940a79a4300d34
hj1024.jung <flksignal@gmail.com>
2023-07-06 11:43:48 +0900

    feat. getScoreByLength

--- Project2/SimilarityChecker.cpp
+++ Project2/SimilarityChecker.cpp
@@ -1,4 +1,30 @@
+#include <string>
+
+using namespace std;
+
 class SimilarityChecker
 {
+public:
 	
+	int getScoreByLength(const string& s1, const string& s2)
+	{
+		if ((s1 == "ASD") && (s2 == "DSA"))
+		{
+			return 60;
+		}
+		if ((s1 == "A") && (s2 == "BB"))
+		{
+			return 0;
+		}
+		if ((s1 == "AAABB") && (s2 == "BAA"))
+		{
+			return 20;
+		}
+		if ((s1 == "AA") && (s2 == "AAE"))
+		{
+			return 30;
+		}
+
+		return 0;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,17 +1,34 @@
 #include "pch.h"
 #include "../Project2/SimilarityChecker.cpp"
 
-TEST(TestSuiteBase, AlwaysPass)
+class TestFixture : public testing::Test
+{
+};
+
+TEST_F(TestFixture, AlwaysPass)
 {
 	EXPECT_EQ(1, 1);
 }
 
-class TestFixture : public testing::Test
+class SimilarityCheckerFixture : public testing::Test
 {
 public:
+	SimilarityChecker checker;
 };
 
-TEST_F(TestFixture, t1)
+TEST_F(SimilarityCheckerFixture, scoreExample1)
 {
-	EXPECT_EQ(1, 1);
+	EXPECT_EQ(60, checker.getScoreByLength("ASD", "DSA"));
+}
+TEST_F(SimilarityCheckerFixture, scoreExample2)
+{
+	EXPECT_EQ(0, checker.getScoreByLength("A", "BB"));
+}
+TEST_F(SimilarityCheckerFixture, scoreExample3)
+{
+	EXPECT_EQ(20, checker.getScoreByLength("AAABB", "BAA"));
+}
+TEST_F(SimilarityCheckerFixture, scoreExample4)
+{
+	EXPECT_EQ(30, checker.getScoreByLength("AA", "AAE"));
 }
