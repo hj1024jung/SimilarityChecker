e75f0aea2df51c8a1f4b950911572035858f0427
hj1024.jung <flksignal@gmail.com>
2023-07-06 12:01:31 +0900

    refac. getScoreByLength

--- Project2/SimilarityChecker.cpp
+++ Project2/SimilarityChecker.cpp
@@ -2,29 +2,31 @@
 
 using namespace std;
 
+int Max(int x, int y) { return (x > y) ? x : y; }
+int Min(int x, int y) { return (x < y) ? x : y; }
+
 class SimilarityChecker
 {
 public:
 	
 	int getScoreByLength(const string& s1, const string& s2)
 	{
-		if ((s1 == "ASD") && (s2 == "DSA"))
+		const int MAX_SCORE = 60;
+
+		int length_s1 = Max(s1.length(), s2.length());
+		int length_s2 = Min(s1.length(), s2.length());
+		int length_gap = (length_s1 - length_s2);
+
+		if(length_s1 == length_s2)
 		{
-			return 60;
+			return MAX_SCORE;
 		}
-		if ((s1 == "A") && (s2 == "BB"))
+
+		if((length_s2 * 2) <= length_s1)
 		{
 			return 0;
 		}
-		if ((s1 == "AAABB") && (s2 == "BAA"))
-		{
-			return 20;
-		}
-		if ((s1 == "AA") && (s2 == "AAE"))
-		{
-			return 30;
-		}
 
-		return 0;
+		return (MAX_SCORE * (length_s2 - length_gap) / length_s2);
 	}
 };
