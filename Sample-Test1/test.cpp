aefffa3014e6c364aaab9a9d38da1bf2ddeccff5
hj1024.jung <flksignal@gmail.com>
2023-07-06 13:39:41 +0900

    feat. getScoreByAlpha

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,17 +1,34 @@
 #include "pch.h"
 #include "../Project2/SimilarityChecker.cpp"
 
-TEST(TestSuiteBase, AlwaysPass)
+class TestFixture : public testing::Test
+{
+};
+
+TEST_F(TestFixture, AlwaysPass)
 {
 	EXPECT_EQ(1, 1);
 }
 
-class TestFixture : public testing::Test
+class SimilarityCheckerFixture : public testing::Test
 {
 public:
+	SimilarityChecker checker;
 };
 
-TEST_F(TestFixture, t1)
+TEST_F(SimilarityCheckerFixture, scoreExample5)
 {
-	EXPECT_EQ(1, 1);
+	EXPECT_EQ(40, checker.getScoreByAlpha("ASD", "DSA"));
+}
+TEST_F(SimilarityCheckerFixture, scoreExample6)
+{
+	EXPECT_EQ(0, checker.getScoreByAlpha("A", "BB"));
+}
+TEST_F(SimilarityCheckerFixture, scoreExample7)
+{
+	EXPECT_EQ(40, checker.getScoreByAlpha("AAABB", "BAA"));
+}
+TEST_F(SimilarityCheckerFixture, scoreExample8)
+{
+	EXPECT_EQ(20, checker.getScoreByAlpha("AA", "AAE"));
 }
